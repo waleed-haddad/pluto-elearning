432be399fa0ac01b98726b7ded613d8e7f09c817
Nikita-B05 <108193728+Nikita-B05@users.noreply.github.com>
2023-12-02 12:55:30 -0500

    Home Page Top
    Created the top of the home page. Title, description, button, and image.

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,3 +1,18 @@
+import avatar from "../assets/home_top_img.png"
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-    return <h1>Home</h1>
+    return(
+      <div className="top_page">
+        <div className="home_top_left">
+          <h1><span className="light_blue_text">Launch</span> Into Financial Mastery Today</h1>
+          <p>Discover, learn, and conquer your path to financial success now!</p>
+          <Link to="/courses"><button>Start Learning</button></Link>
+        </div>
+        
+        <div className="home_top_right">
+          <img src={avatar} alt="Home Avatar Image" width="700px"/>
+        </div>
+      </div>
+    )
   }
